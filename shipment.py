1f02afba33d556591de08aaef40e601ee4a1ace1
Raimon Esteve <raimon@nan-tic.com>
2017-06-09 10:47:41 +0200

    Replace shipment qty decimal from Double

--- shipment.py
+++ shipment.py
@@ -1,12 +1,28 @@
-# This file is part jasper_reports module for Tryton.
+# This file is part stock_delivery_note_valuer_jreport module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond.pool import Pool
 from trytond.pool import PoolMeta
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['DeliveryNoteValued']
-__metaclass__ = PoolMeta
 
 
 class DeliveryNoteValued(JasperReport):
+    __metaclass__ = PoolMeta
     __name__ = 'stock.shipment.out.delivery_note_valued'
+
+    @classmethod
+    def execute(cls, ids, data):
+        pool = Pool()
+        Config = pool.get('stock.configuration')
+
+        config = Config(1)
+        parameters = {
+            'shipment_qty_decimal': config.shipment_qty_decimal or False
+            }
+        if 'parameters' in data:
+            data['parameters'].update(parameters)
+        else:
+            data['parameters'] = parameters
+        return super(DeliveryNoteValued, cls).execute(ids, data)
